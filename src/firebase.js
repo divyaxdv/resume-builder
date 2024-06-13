f43cb9fcff361463e20d62038538173f12462de3
Divya <divyaxdv@gmail.com>
2024-06-14 04:02:10 +0530

    added .env file

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyANnQu8NyXXddCDiXMlo71Vwn-KUPqjNAA",
-  authDomain: "resume-builder-c9c01.firebaseapp.com",
-  projectId: "resume-builder-c9c01",
-  storageBucket: "resume-builder-c9c01.appspot.com",
-  messagingSenderId: "447338692579",
-  appId: "1:447338692579:web:14e2d4dcbbf6a37eeaaad6"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
